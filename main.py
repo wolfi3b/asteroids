707ae1d436d447867d1a71d91381ec0f244f6358
Tomas Zachar <tomas.zachar@enverus.com>
2025-03-25 09:48:18 +0100

    - setup

---
+++ main.py
@@ -0,0 +1,13 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+	print("Starting Asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+	main()
